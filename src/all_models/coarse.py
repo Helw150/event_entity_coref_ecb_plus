632dbb34043b55c856166315a16d31778c583699
helw150 <me@williamheld.com>
2020-12-10 07:16:47 +0000

    Exemplar Bi-encoder Training

--- src/all_models/coarse.py
+++ src/all_models/coarse.py
@@ -5,50 +5,78 @@ import torch.autograd as autograd
 from transformers import RobertaModel
 from tqdm import tqdm, trange
 
+
 class EncoderCosineRanker(nn.Module):
     def __init__(self, device):
         super(EncoderCosineRanker, self).__init__()
         self.device = device
         self.model_type = 'EncoderCosineRanker'
-        self.mention_model = RobertaModel.from_pretrained('roberta-base', return_dict=True)
+        self.mention_model = RobertaModel.from_pretrained('roberta-base',
+                                                          return_dict=True)
         self.cluster_lookup = {}
 
     def update_cluster_lookup(self, label_sets):
         self.cluster_lookup = {}
         with torch.no_grad():
-            for label_id, label_exemplar in tqdm(label_sets.items(), desc="Exemplar Reps"):
-                assert(label_exemplar["label"] == label_id)
-                sentences = torch.tensor(label_exemplar["sentence"]).unsqueeze(1).permute(1, 0)
+            for label_id, label_exemplar in tqdm(label_sets.items(),
+                                                 desc="Exemplar Reps"):
+                assert (label_exemplar["label"] == [label_id])
+                sentences = torch.tensor(
+                    label_exemplar["sentence"]).unsqueeze(1).permute(1, 0).to(
+                        self.device)
                 transformer_output = self.get_sentence_vecs(sentences)
-                start = torch.tensor(label_exemplar["start_piece"]).unsqueeze(1)
-                end = torch.tensor(label_exemplar["end_piece"]).unsqueeze(1)
-                mention_rep = self.get_mention_rep(transformer_output, start, end)
+                start = torch.tensor(
+                    label_exemplar["start_piece"]).unsqueeze(1).to(self.device)
+                end = torch.tensor(
+                    label_exemplar["end_piece"]).unsqueeze(1).to(self.device)
+                mention_rep = self.get_mention_rep(transformer_output, start,
+                                                   end)
                 self.cluster_lookup[label_id] = mention_rep
 
     def get_sentence_vecs(self, sentences):
         expected_transformer_input = self.to_transformer_input(sentences)
-        transformer_output = self.mention_model(**expected_transformer_input).last_hidden_state
+        transformer_output = self.mention_model(
+            **expected_transformer_input).last_hidden_state
         return transformer_output
-            
+
     def get_mention_rep(self, transformer_output, start_pieces, end_pieces):
-        start_piece_vec = torch.gather(transformer_output, 1, start_pieces.unsqueeze(1).repeat(transformer_output.shape[0], 1, 768))
-        end_piece_vec = torch.gather(transformer_output, 1, end_pieces.unsqueeze(1).repeat(transformer_output.shape[0], 1, 768))
-        mention_rep = torch.cat([start_piece_vec, end_piece_vec], dim = 2)
+        start_pieces = start_pieces.repeat(1, 768).view(-1, 1, 768)
+        start_piece_vec = torch.gather(transformer_output, 1, start_pieces)
+        end_piece_vec = torch.gather(
+            transformer_output, 1,
+            end_pieces.repeat(1, 768).view(-1, 1, 768))
+
+        mention_rep = torch.cat([start_piece_vec, end_piece_vec], dim=2)
         return mention_rep
-    
+
     def to_transformer_input(self, sentence_tokens):
         segment_idx = sentence_tokens * 0
         mask = sentence_tokens != 0
-        return {"input_ids" : sentence_tokens, "token_type_ids" : segment_idx, "attention_mask" : mask}
+        return {
+            "input_ids": sentence_tokens,
+            "token_type_ids": segment_idx,
+            "attention_mask": mask
+        }
 
     def convert_labels_to_reps(self, label):
         return self.cluster_lookup[label[0]]
 
     def forward(self, sentences, start_pieces, end_pieces, labels):
-        exemplars = torch.tensor(list(map(self.convert_labels_to_reps, labels.cpu().tolist()))).to(self.device)
+        exemplars = list(
+            map(self.convert_labels_to_reps,
+                labels.cpu().tolist()))
+        exemplars = torch.cat(exemplars, dim=0)
         transformer_output = self.get_sentence_vecs(sentences)
-        mention_reps = self.get_mention_rep(transformer_output, start_pieces, end_pieces)
-        exemplar_norm = exemplar / exemplar.norm(dim=2)[:, None]
-        mention_reps_norm = mention_reps / mention_reps.norm(dim=2)[:, None]
-        similarities = torch.mm(exemplar_norm, mention_reps_norm.transpose(0,1))
-        print(similarities)
+        mention_reps = self.get_mention_rep(transformer_output, start_pieces,
+                                            end_pieces)
+        exemplars = exemplars.squeeze(1)
+        mention_reps = mention_reps.squeeze(1)
+        scores = torch.mm(exemplars, mention_reps.t()).view(-1, 1)
+
+        pairwise_labels = (labels == labels.t()).view(-1, 1).type(
+            torch.FloatTensor).to(self.device)
+
+        loss_fct = nn.BCEWithLogitsLoss(reduction="mean")
+        loss = loss_fct(scores, pairwise_labels)
+
+        return loss, scores
--- src/all_models/train_model.py
+++ src/all_models/train_model.py
@@ -16,9 +16,11 @@ sys.path.append("/src/shared/")
 
 parser = argparse.ArgumentParser(description='Training a regressor')
 
-parser.add_argument('--config_path', type=str,
+parser.add_argument('--config_path',
+                    type=str,
                     help=' The path configuration json file')
-parser.add_argument('--out_dir', type=str,
+parser.add_argument('--out_dir',
+                    type=str,
                     help=' The directory to the output folder')
 
 args = parser.parse_args()
@@ -28,15 +30,15 @@ if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
 logging.basicConfig(filename=os.path.join(args.out_dir, "train_log.txt"),
-                    level=logging.DEBUG, filemode='w')
+                    level=logging.DEBUG,
+                    filemode='w')
 
 # Load json config file
 with open(args.config_path, 'r') as js_file:
     config_dict = json.load(js_file)
 
-with open(os.path.join(args.out_dir,'train_config.json'), "w") as js_file:
+with open(os.path.join(args.out_dir, 'train_config.json'), "w") as js_file:
     json.dump(config_dict, js_file, indent=4, sort_keys=True)
-
 
 if config_dict["gpu_num"] != -1:
     os.environ["CUDA_VISIBLE_DEVICES"] = str(config_dict["gpu_num"])
@@ -68,14 +70,13 @@ if args.use_cuda:
 
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 max_seq_length = 256
-    
+
+
 def tokenize_and_map(sentence):
     embeddings = tokenizer(sentence.get_raw_sentence())["input_ids"]
     counter = 0
     mapping = {k: [] for k, _ in enumerate(sentence.get_tokens_strings())}
-    for i, token in enumerate(
-            tokenizer.convert_ids_to_tokens(
-                embeddings)):
+    for i, token in enumerate(tokenizer.convert_ids_to_tokens(embeddings)):
         if token == "<s>" or token == "</s>":
             continue
         elif token[0] == "Ġ":
@@ -88,50 +89,67 @@ def tokenize_and_map(sentence):
     embeddings += padding
     return embeddings, mapping
 
+
 def structure_dataset(data_set, events=True):
     processed_dataset = []
     labels_to_ids = {}
     label_sets = {}
     label_vocab_size = 0
-    docs = [document for topic in data_set.topics.values() for document in topic.docs.values()]
+    docs = [
+        document for topic in data_set.topics.values()
+        for document in topic.docs.values()
+    ]
     for doc in docs:
         for sentence in doc.get_sentences().values():
-            tokenized_sentence, tokenization_mapping = tokenize_and_map(sentence)
+            tokenized_sentence, tokenization_mapping = tokenize_and_map(
+                sentence)
             sentence_mentions = sentence.gold_event_mentions if events else sentence.gold_entity_mentions
             for mention in sentence_mentions:
                 if mention.gold_tag not in labels_to_ids:
                     labels_to_ids[mention.gold_tag] = label_vocab_size
                     exemplar = True
-                    label_vocab_size += 1 
+                    label_vocab_size += 1
                 label_id = labels_to_ids[mention.gold_tag]
                 start_piece = tokenization_mapping[mention.start_offset][0]
                 end_piece = tokenization_mapping[mention.end_offset][-1]
-                record = {"sentence" : tokenized_sentence,
-                          "label" : label_id,
-                          "start_piece": [start_piece],
-                          "end_piece" : [end_piece]}
+                record = {
+                    "sentence": tokenized_sentence,
+                    "label": [label_id],
+                    "start_piece": [start_piece],
+                    "end_piece": [end_piece]
+                }
                 processed_dataset.append(record)
                 if exemplar:
                     label_sets[label_id] = record
                     exemplar = False
 
-    sentences = torch.tensor([record["sentence"] for record in processed_dataset])
+    sentences = torch.tensor(
+        [record["sentence"] for record in processed_dataset])
     labels = torch.tensor([record["label"] for record in processed_dataset])
-    start_pieces = torch.tensor([record["start_piece"] for record in processed_dataset])
-    end_pieces = torch.tensor([record["end_piece"] for record in processed_dataset])
-    return TensorDataset(sentences, labels, start_pieces, end_pieces), label_sets
+    start_pieces = torch.tensor(
+        [record["start_piece"] for record in processed_dataset])
+    end_pieces = torch.tensor(
+        [record["end_piece"] for record in processed_dataset])
+    return TensorDataset(sentences, start_pieces, end_pieces,
+                         labels), label_sets
+
 
 def get_optimizer(model):
     lr = config_dict["lr"]
     optimizer = None
-    parameters = filter(lambda p: p.requires_grad,model.parameters())
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
     if config_dict["optimizer"] == 'adadelta':
-        optimizer = optim.Adadelta(parameters, lr=lr,
+        optimizer = optim.Adadelta(parameters,
+                                   lr=lr,
                                    weight_decay=config_dict["weight_decay"])
     elif config_dict["optimizer"] == 'adam':
-        optimizer = optim.Adam(parameters, lr=lr, weight_decay=config_dict["weight_decay"])
+        optimizer = optim.Adam(parameters,
+                               lr=lr,
+                               weight_decay=config_dict["weight_decay"])
     elif config_dict["optimizer"] == 'sgd':
-        optimizer = optim.SGD(parameters, lr=lr, momentum=config_dict["momentum"],
+        optimizer = optim.SGD(parameters,
+                              lr=lr,
+                              momentum=config_dict["momentum"],
                               nesterov=True)
 
     assert (optimizer is not None), "Config error, check the optimizer field"
@@ -146,33 +164,53 @@ def get_scheduler(optimizer, len_train_data):
     num_train_steps = int(len_train_data / batch_size) * epochs
     num_warmup_steps = int(num_train_steps * config_dict["warmup_proportion"])
 
-    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_train_steps)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps,
+                                                num_train_steps)
     return scheduler
+
 
 def train_model(train_set, dev_set):
     device = torch.device("cuda:0" if args.use_cuda else "cpu")
     model = EncoderCosineRanker(device)
     model.to(device)
-    train_event_mentions, train_event_gold = structure_dataset(train_set, events = True)
+    train_event_mentions, train_event_gold = structure_dataset(train_set,
+                                                               events=True)
     #train_entity_mentions, train_entity_gold = structure_dataset(train_set, events = False)
-    dev_event_mentions, dev_event_gold = structure_dataset(dev_set, events = True)
+    dev_event_mentions, dev_event_gold = structure_dataset(dev_set,
+                                                           events=True)
     #dev_entity_mentions, dev_entity_gold = structure_dataset(dev_set, events = False)
     optimizer = get_optimizer(model)
     scheduler = get_scheduler(optimizer, len(train_event_mentions))
     train_sampler = RandomSampler(train_event_mentions)
-    train_dataloader = DataLoader(
-        train_event_mentions, sampler=train_sampler, batch_size=config_dict["batch_size"]
-    )
+    train_dataloader = DataLoader(train_event_mentions,
+                                  sampler=train_sampler,
+                                  batch_size=config_dict["batch_size"])
 
     model.train()
     for epoch_idx in trange(int(config_dict["epochs"]), desc="Epoch"):
+        tr_loss = 0.0
         model.update_cluster_lookup(train_event_gold)
         for step, batch in enumerate(tqdm(train_dataloader, desc="Batch")):
             batch = tuple(t.to(device) for t in batch)
             sentences, start_pieces, end_pieces, labels = batch
-            model(sentences, start_pieces, end_pieces, labels)
-            
-    
+            loss, _ = model(sentences, start_pieces, end_pieces, labels)
+            loss.backward()
+            tr_loss += loss.item()
+
+            if (step + 1) * config_dict["batch_size"] % config_dict[
+                    "accumulated_batch_size"] == 0:
+                tqdm.write("Step {} - epoch {} average loss: {:.6f}\n".format(
+                    step,
+                    epoch_idx,
+                    tr_loss / float(config_dict["accumulated_batch_size"]),
+                ))
+                tr_loss = 0
+                torch.nn.utils.clip_grad_norm_(model.parameters(),
+                                               config_dict["max_grad_norm"])
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+
 
 def main():
     logging.info('Loading training and dev data...')
